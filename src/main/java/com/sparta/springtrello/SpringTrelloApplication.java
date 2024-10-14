83284c3ce81afe35464e19d06a271b2b69603827
qowlsrhks <skswlsrhks@naver.com>
2024-10-14 17:40:43 +0900

    first commit

---
+++ src/main/java/com/sparta/springtrello/SpringTrelloApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.springtrello.springtrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrelloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTrelloApplication.class, args);
+    }
+
+}
