b299374f3da60aadd54bf26503ddd4ff24f2ab6f
qowlsrhks <skswlsrhks@naver.com>
2024-10-14 18:10:08 +0900

    first commit

--- src/main/java/com/sparta/springtrello/SpringTrelloApplication.java
+++ src/main/java/com/sparta/springtrello/SpringTrelloApplication.java
@@ -1,9 +1,10 @@
-package com.sparta.springtrello.springtrello;
+package com.sparta.springtrello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SpringTrelloApplication {
 
     public static void main(String[] args) {
