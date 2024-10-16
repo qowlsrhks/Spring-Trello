4b752a5a2aef89cfc04876ce749c270cfda1cdac
qowlsrhks <skswlsrhks@naver.com>
2024-10-17 03:44:52 +0900

    add :  findByWorkSpaceAndUser 추가

--- src/main/java/com/sparta/springtrello/domain/workspace/repository/WorkspaceMemberRepository.java
+++ src/main/java/com/sparta/springtrello/domain/workspace/repository/WorkspaceMemberRepository.java
@@ -1,16 +1,22 @@
 package com.sparta.springtrello.domain.workspace.repository;
 
 
+import com.sparta.springtrello.domain.user.entity.User;
 import com.sparta.springtrello.domain.workspace.entity.MemberRole;
+import com.sparta.springtrello.domain.workspace.entity.WorkSpace;
 import com.sparta.springtrello.domain.workspace.entity.WorkspaceMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface WorkspaceMemberRepository extends JpaRepository<WorkspaceMember, Long> {
     List<WorkspaceMember>findByUserEmail(String email);
     List<WorkspaceMember>findByUserEmailAndWorkSpace_WorkspaceId(String email ,Long workSpaceId);
     List<WorkspaceMember> findByRole(MemberRole role);
+
+    Optional<WorkspaceMember> findByWorkSpaceAndUser(WorkSpace workSpace, User user);
+
 }
