445a31e11a02160f11733fe831f8f01de4ddab0d
Aakaive <wonseok031@gmail.com>
2024-10-17 15:43:13 +0900

    - AOP 포인트컷 별 개별 메세지 추가

--- src/main/java/com/sparta/springtrello/aop/NotificationAspect.java
+++ src/main/java/com/sparta/springtrello/aop/NotificationAspect.java
@@ -16,11 +16,29 @@ public class NotificationAspect {
     }
 
 //    @AfterReturning(pointcut = "execution(* com.sparta.springtrello..*.*(..)) && @annotation(org.springframework.web.bind.annotation.PostMapping)", returning = "result")
-    @AfterReturning(pointcut = "execution(* com.sparta.springtrello.domain.board.controller.BoardController.createBoard(..)) || "
-            + "execution(* com.sparta.springtrello.domain.comment.controller.CommentController.createComment(..))", returning = "result")
+    @AfterReturning(pointcut = "execution(* com.sparta.springtrello.domain.comment.controller.CommentController.createComment(..)) || "
+            + "execution(* com.sparta.springtrello.domain.workspace.controller.WorkspaceController.addMember(..)) || "
+            + "execution(* com.sparta.springtrello.domain.card.controller.CardController.createCard(..)) ||"
+            + "execution(* com.sparta.springtrello.domain.card.controller.CardController.updateCard(..))", returning = "result")
     public void sendNotification(JoinPoint joinPoint, Object result) {
         // 슬랙 알림 메시지 생성
+        String methodName = joinPoint.getSignature().getName();
         String message = "API 호출: " + joinPoint.getSignature().getName();
+        notificationService.sendSlackNotification(message);
+
+        if(methodName.equals("createCard")) {
+            message = "새로운 카드가 생성되었습니다.";
+        }
+        else if(methodName.equals("addMember")) {
+            message = "멤버가 추가되었습니다.";
+        }
+        else if(methodName.equals("updateCard")) {
+            message = "카드가 수정되었습니다.";
+        }
+        else if(methodName.equals("createComment")) {
+            message = "댓글이 작성되었습니다.";
+        }
+
         notificationService.sendSlackNotification(message);
 
         //if나 스위치로 구현 -> 스위치가 나을듯?
